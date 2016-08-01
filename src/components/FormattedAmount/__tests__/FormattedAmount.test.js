e48a106a0769943550db5ecf9168d6c0dff76ee4
Julien Tassin <jtassin@artprice.com>
2016-08-01 11:27:20 +0200

    add unit tests

--- src/components/FormattedAmount/__tests__/FormattedAmount.test.js
+++ src/components/FormattedAmount/__tests__/FormattedAmount.test.js
@@ -1,27 +1,39 @@
 import React from 'react';
 import { expect } from 'chai';
 import { shallow } from 'enzyme';
-import FormattedAmount from '../FormattedAmount';
+import FormattedAmount from '../../FormattedAmount';
 
 describe('FormattedAmount', () => {
 
-  it('0 pad amount and add € symbol', () => {
+  it('0 pad amount and add the correct currency symbol', () => {
     let wrapper = shallow(
-      <FormattedAmount amount={200} />
+      <FormattedAmount amount={200} currency='€'/>
     );
     expect(wrapper.html()).to.equal('<span>2.00 €</span>');
+    
     wrapper = shallow(
-      <FormattedAmount amount={210} />
+      <FormattedAmount amount={210} currency='$'/>
     );
-    expect(wrapper.html()).to.equal('<span>2.10 €</span>');
+    expect(wrapper.html()).to.equal('<span>2.10 $</span>');
+    
     wrapper = shallow(
-      <FormattedAmount amount={211} />
+      <FormattedAmount amount={211} currency='€'/>
     );
     expect(wrapper.html()).to.equal('<span>2.11 €</span>');
+    
     wrapper = shallow(
-      <FormattedAmount amount={211.1} />
+      <FormattedAmount amount={211.1} currency='€'/>
     );
     expect(wrapper.html()).to.equal('<span>2.11 €</span>');
 
+  });
+
+  it('display the amount in red with parenthesis if negative amount', () => {
+    let wrapper = shallow(
+        <FormattedAmount amount={-200} currency='€'/>
+    );
+    expect(wrapper.html()).to.equal('<span style="color:red;">(2.00 €)</span>');
+    
+    
   });
 });
---
+++ src/components/FormattedAmount/index.js
@@ -0,0 +1,36 @@
+import React, { PropTypes, Component } from 'react';
+import PureRenderMixin from 'react-addons-pure-render-mixin';
+
+class FormattedAmount extends Component {
+
+    static propTypes = {
+        amount: PropTypes.number.isRequired,
+        currency: PropTypes.string.isRequired,
+    };
+
+    constructor(props) {
+        super(props);
+        this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
+    }
+
+    render() {
+        let result = '';
+        const styles = {
+            negative: {
+                color: 'red',
+            },
+        };
+        if (this.props.amount) {
+            if (this.props.amount > 0) {
+                result = <span>{(this.props.amount / 100).toFixed(2)} {this.props.currency}</span>;
+            } else {
+                result = <span style={styles.negative}>({(-this.props.amount / 100).toFixed(2)} {this.props.currency})</span>;
+            }
+        } else {
+            result = <span>0.00 €</span>;
+        }
+        return result;
+    }
+}
+
+export default FormattedAmount;
