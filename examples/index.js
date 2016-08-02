7e3e81893a9d21a7be5102fff576f17d582df117
Julien Tassin <jtassin@artprice.com>
2016-08-02 10:53:44 +0200

    lib is now dist

--- examples/index.js
+++ examples/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import FormattedAmount from '../../lib/index.js';
+import FormattedAmount from '../../dist/index.js';
 
 ReactDOM.render(<FormattedAmount amount={1337} currency={'€'} />, document.getElementById('euro-positive-container'));
 
